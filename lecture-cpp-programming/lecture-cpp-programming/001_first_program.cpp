96d5f7e1fa112aa2ea6f08b68e4ce9c94b60180f
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-05-15 09:12:30 +0700

    refactor: project architecture

---
+++ lecture-cpp-programming/lecture-cpp-programming/001_first_program.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+int firstProgram() {
+	// sum of 2 number
+	int a, b, c;
+
+	std::cout << "Enter 2 numbers: ";
+	std::cin >> a >> b; // รับ input มาจาก keyboard
+
+	c = a + b;
+
+	std::cout << "Sum of values is " << c << std::endl;
+
+	// welcome user
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::getline(std::cin, name);
+	std::cout << "Welcome " << name << std::endl;
+
+	return 0;
+}
