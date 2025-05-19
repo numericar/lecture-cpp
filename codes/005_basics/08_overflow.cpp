ab9a5b3dc611b49e7d5e83c1a18bac831e3f9807
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-05-19 09:00:56 +0700

    exam: overflow

--- codes/005_basics/08_overflow.cpp
+++ codes/005_basics/08_overflow.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 int main() {
-    char x = 127;
+    char x = 129; // x = 129, result will bwe -127
+    std::cout << (int) x << std::endl;
 
-    std::cout << x << std::endl;
+    char y = -128;
+    std::cout << (int) --y << std::endl;
 
     return 0;
 }
