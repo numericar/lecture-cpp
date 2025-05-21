19802dba0ef3a34f58d1ca809329ecadfe58fbb9
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-05-21 09:05:27 +0700

    lecture: if statement

---
+++ codes/006_condition_statement/01_if_statement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // finding maximum of 2 number
+    int a = 5, b = 10;
+    if (a > b) 
+    {
+        std::cout << "a more than b" << std::endl;
+    } 
+    else 
+    {
+        std::cout << "b more than a" << std::endl;
+    }
+    
+    return 0;
+}
